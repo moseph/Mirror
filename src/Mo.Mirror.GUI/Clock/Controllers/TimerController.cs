1cd65e31a1a294a7200135038541d2f5922f7a3c
Mo <mohummedmujir@gmail.com>
2016-02-19 21:39:57 +0000

    Created TimerController

---
+++ src/Mo.Mirror.GUI/Clock/Controllers/TimerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Mo.Mirror.GUI.Clock.Controllers
+{
+    static class TimerController
+    {
+        public static void RegisterModel(BaseModel model)
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = model.Inteval;
+            timer.Tick += model.TimerTick;
+            timer.Start();
+        }
+    }
+}
